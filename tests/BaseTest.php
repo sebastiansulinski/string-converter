8d30771e6d764c382379440421bce5ae3cd6a796
Sebastian Sulinski <Sebastian@Sebastians-Mac-Pro.local>
2015-02-28 23:39:37 +0000

    Initial commit

---
+++ tests/BaseTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use PHPUnit_Framework_TestCase;
+
+
+abstract class BaseTest extends PHPUnit_Framework_TestCase {
+
+    const STRING_CAMEL = 'phpEol';
+
+    const STRING_CONSTANT = 'PHP_EOL';
+
+    const STRING_HYPHEN = 'php-eol';
+
+    const STRING_UNDERSCORE = 'php_eol';
+
+}
