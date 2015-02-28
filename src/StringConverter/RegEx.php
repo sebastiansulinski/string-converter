dc5dee6078a58b797d6faf13b45541d5d0f9780d
Sebastian Sulinski <Sebastian@Sebastians-Mac-Pro.local>
2015-02-28 23:38:55 +0000

    Initial commit

---
+++ src/StringConverter/RegEx.php
@@ -0,0 +1,12 @@
+<?php namespace SSD\StringConverter;
+
+
+class RegEx {
+
+    const REGEX_UNDERSCORE = '/_(.)/';
+
+    const REGEX_HYPHEN = '/-(.)/';
+
+    const REGEX_CAPITAL_LETTERS = '/([A-Z])/';
+
+}
