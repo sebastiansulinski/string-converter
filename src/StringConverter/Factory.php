40be943dcd26b6b96397441531b1d3a5290b8d38
Sebastian Sulinski <Sebastian@Sebastians-Mac-Pro.local>
2015-02-28 23:38:37 +0000

    Added new logic for class::method extraction

--- src/StringConverter/Factory.php
+++ src/StringConverter/Factory.php
@@ -8,35 +8,124 @@ class Factory {
 
 
     /**
-     * Return ReflectionClass instance of the Converter class.
+     * Return ReflectionClass instance of the given class.
+     *
+     * @param $className
      *
      * @return ReflectionClass
      */
-    private static function reflection()
+    private static function reflection($className)
     {
 
-        return (new ReflectionClass(new Converter));
+        return (new ReflectionClass(new $className));
     }
 
 
     /**
-     * Call the method statically on the Converter class instance.
+     * Return formatted class name.
      *
-     * @param $method
+     * @param $name
+     *
+     * @return string
+     */
+    private static function className($name)
+    {
+
+        return "SSD\\StringConverter\\Types\\".ucfirst($name);
+
+    }
+
+
+    /**
+     * Return formatted method name.
+     *
+     * @param $elements
+     *
+     * @return string
+     */
+    private static function methodName($elements)
+    {
+
+        return lcfirst(implode($elements));
+
+    }
+
+
+    /**
+     * Split name into array using capital letters as delimiter.
+     *
+     * @param $name
+     *
+     * @return array
+     */
+    private static function splitName($name)
+    {
+
+        return preg_split(
+            RegEx::REGEX_CAPITAL_LETTERS,
+            $name,
+            -1,
+            PREG_SPLIT_DELIM_CAPTURE
+        );
+
+    }
+
+
+    /**
+     * Get array with index 0 representing class name
+     * and index 1 representing the method name.
+     *
+     * @param $name
+     *
+     * @return array
+     */
+    private static function getClassMethod($name)
+    {
+
+        $elements = static::splitName($name);
+
+        if (count($elements) < 2) {
+
+            throw new BadMethodCallException(
+                "The method you called does not exist"
+            );
+
+        }
+
+        return [
+            static::className(array_shift($elements)),
+            static::methodName($elements)
+        ];
+
+    }
+
+
+    /**
+     * Call the method statically on the Converter sub-class.
+     *
+     * @param $name
      * @param $arguments
      *
      * @return mixed
      */
-    public static function __callStatic($method, array $arguments = [])
+    public static function __callStatic($name, array $arguments = [])
     {
 
-        if ( ! static::reflection()->hasMethod($method)) {
+        list($className, $method) = static::getClassMethod($name);
 
-            throw new BadMethodCallException("Method {$method} does not exist");
+
+        if ( ! static::reflection($className)->hasMethod($method)) {
+
+            throw new BadMethodCallException(
+                "Method {$method} does not exist on class {$className}"
+            );
 
         }
 
-        return call_user_func_array([new Converter, $method], $arguments);
+        return call_user_func_array(
+            [new $className, $method],
+            $arguments
+        );
 
     }
 
