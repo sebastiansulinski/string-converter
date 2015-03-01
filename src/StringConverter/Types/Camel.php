69f204be15dbc5b610a6c800e4832906fb2a177d
Sebastian Sulinski <Sebastian@Sebastians-Mac-Pro.local>
2015-03-01 07:56:56 +0000

    Pushed methods up

--- src/StringConverter/Types/Camel.php
+++ src/StringConverter/Types/Camel.php
@@ -15,7 +15,7 @@ class Camel extends Converter {
      *
      * @return mixed
      */
-    private function express($string, $method)
+    protected function express($string, $method)
     {
 
         return preg_replace_callback(
@@ -26,48 +26,5 @@ class Camel extends Converter {
 
     }
 
-    /**
-     * Convert to upper-case underscore.
-     *
-     * @param $string
-     *
-     * @return string
-     */
-    public function toConstant($string)
-    {
-
-        return strtoupper($this->express($string, 'underscore'));
-
-    }
-
-
-    /**
-     * Convert to lower-case hyphen.
-     *
-     * @param $string
-     *
-     * @return string
-     */
-    public function toHyphen($string)
-    {
-
-        return strtolower($this->express($string, 'hyphen'));
-
-    }
-
-
-    /**
-     * Convert to lower-case underscore.
-     *
-     * @param $string
-     *
-     * @return string
-     */
-    public function toUnderscore($string)
-    {
-
-        return strtolower($this->express($string, 'underscore'));
-
-    }
 
 }
