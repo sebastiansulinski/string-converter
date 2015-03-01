5a4c1e5693c6d1a7a0fadb6f780b3a1d7f6d9d34
Sebastian Sulinski <Sebastian@Sebastians-Mac-Pro.local>
2015-03-01 07:53:06 +0000

    Pulled common methods and added abstract express()

--- src/StringConverter/Converter.php
+++ src/StringConverter/Converter.php
@@ -49,4 +49,116 @@ abstract class Converter {
     }
 
 
+    /**
+     * Prepend space.
+     *
+     * @param $params
+     *
+     * @return string
+     */
+    final protected function space($params)
+    {
+
+        return " " . $params[1];
+    }
+
+
+    /**
+     * Return result of the regular expression replacement
+     *
+     * @param $string
+     * @param $methodName
+     *
+     * @return mixed
+     */
+    abstract protected function express($string, $methodName);
+
+
+    /**
+     * Convert hyphens to camel case.
+     *
+     * @param $string
+     *
+     * @return mixed
+     */
+    public function toCamel($string)
+    {
+
+        return $this->express(strtolower($string), 'upperCaseFirst');
+
+    }
+
+
+    /**
+     * Convert to upper-case underscore.
+     *
+     * @param $string
+     *
+     * @return string
+     */
+    public function toConstant($string)
+    {
+
+        return strtoupper($this->express($string, 'underscore'));
+
+    }
+
+
+    /**
+     * Convert to lower-case hyphen.
+     *
+     * @param $string
+     *
+     * @return mixed
+     */
+    public function toHyphen($string)
+    {
+
+        return strtolower($this->express($string, 'hyphen'));
+
+    }
+
+
+    /**
+     * Convert to lower-case underscore.
+     *
+     * @param $string
+     *
+     * @return string
+     */
+    public function toUnderscore($string)
+    {
+
+        return strtolower($this->express($string, 'underscore'));
+
+    }
+
+
+    /**
+     * Convert to space.
+     *
+     * @param $string
+     * @param $function
+     *
+     * @return mixed
+     */
+    public function toSpace($string, $function = null)
+    {
+
+        $string = $this->express($string, 'space');
+
+        if (empty($function)) {
+
+            return $string;
+
+        }
+
+        return call_user_func(
+            $function,
+            $string
+        );
+
+    }
+
+
 }
